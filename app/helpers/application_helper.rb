40b56d18a2b3e5b5e8935df25f3e9fbf045e82e0
Sean Sellek <me@seansellek.com>
2016-02-07 15:44:14 -0500

    Fix namespacing

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module WyncodeHelpers
-  module ApplicationHelper
-    def render_markdown (raw_string, opts = {tag: "div"})
-      <<-HTML.html_safe
-        <#{opts[:tag]} class="marked-render" data-raw="#{h raw_string}"></#{opts[:tag]}>
-      HTML
-    end
-  end
-end
--- lib/wyncode_assets/version.rb
+++ lib/wyncode_assets/version.rb
@@ -1,3 +1,3 @@
 module WyncodeAssets
-  VERSION = "0.3.1"
+  VERSION = "0.3.1a"
 end
