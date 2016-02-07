89d1b2307b823c0a763aab34dcd21086dfaedfd8
Sean Sellek <me@seansellek.com>
2016-02-07 15:58:35 -0500

    Fixed issue with options hash

--- app/helpers/wyncode_helpers/application_helper.rb
+++ app/helpers/wyncode_helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module WyncodeHelpers
   module ApplicationHelper
-    def render_markdown(raw_string, opts = {tag: "div"})
+    def render_markdown(raw_string, opts = {})
+      tag = opts[:tag] || "div"
       classes = ["marked-render"]
       classes << opts[:class] if opts[:class]
       classes = classes.join(" ")
       id = opts[:id] || ""
       <<-HTML.html_safe
-        <#{opts[:tag]} class="#{classes}" id="#{id}" data-raw="#{h raw_string}"></#{opts[:tag]}>
+        <#{tag} class="#{classes}" id="#{id}" data-raw="#{h raw_string}"></#{tag}>
       HTML
     end
   end
--- lib/wyncode_assets/version.rb
+++ lib/wyncode_assets/version.rb
@@ -1,3 +1,3 @@
 module WyncodeAssets
-  VERSION = "0.3.1d"
+  VERSION = "0.3.2"
 end
